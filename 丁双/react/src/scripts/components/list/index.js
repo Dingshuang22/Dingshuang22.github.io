8aa136eade0de2ecf38cb52ea0d40249feea8c9d
dingshuang <Dingshuang>
2019-07-14 18:52:49 +0800

    react2

--- 丁双/react/src/scripts/components/list/index.js
+++ 丁双/react/src/scripts/components/list/index.js
@@ -21,30 +21,6 @@ export class GoodList extends Component{
         // console.log(goods);
         return (
             <div>
-                <PullToRefresh
-                    damping={60}
-                    ref={el => this.ptr = el}
-                    style={{
-                    height: this.state.height,
-                    overflow: 'auto',
-                    }}
-                    indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
-                    direction='down'
-                    refreshing={this.state.refreshing}
-                    onRefresh={() => {
-                        // 表示正在刷新
-                    this.setState({ refreshing: true });
-                    setTimeout(() => {
-                        this.setState({ refreshing: false });
-                        
-                        // 列表商品翻转
-                        this.props.dispatch(reverseGoods());
-
-                        // 搜索翻转
-                            this.props.dispatch(reverseSearchGoods());
-                    }, 1000);
-                    }}
-                >
                      {
                         goods.map((item,i)=>{
                             return (
@@ -54,9 +30,6 @@ export class GoodList extends Component{
                             )
                         })
                     }
-                </PullToRefresh>
-                
-               
             </div>
         )
     }
